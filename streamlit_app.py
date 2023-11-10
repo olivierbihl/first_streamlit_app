13fb994cd9662763d1a6710fedccd9b6a009f2d8
olivierbihl <57072989+olivierbihl@users.noreply.github.com>
2023-11-10 14:02:30 -0500

    Update streamlit_app.py
    spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
