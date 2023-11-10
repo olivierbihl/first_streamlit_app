e674a824b4e39e789f0e7778df12736bf3d08725
olivierbihl <57072989+olivierbihl@users.noreply.github.com>
2023-11-10 13:56:45 -0500

    Update streamlit_app.py
    add text no typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range egg')
